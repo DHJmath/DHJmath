c36ae9b626eb024c8bafaf8f99894669b4768a6d
노다혜 <ddhh3@naver.com>
2021-08-29 18:24:18 +0900

    <!-- 네이버 & 카카오로 로그인 구현 -->
    1. 네이버 & 카카오 API 등록
    서비스 등록 → 여기서 인증정보(cilentId 와 cilentSecret)를 발급받고 시작해야함
    
    2. application-oauth 등록
    클라이언트 id 비번 등록
    registration 값과 provider 값 등록
    
    3. 스프링 시큐리티 설정
    OAuthAttributes에 네이버인지 판단하는 코드와 생성자 추가
    
    4. 로그인 화면 구현
    user-login.mustache

--- src/main/java/com/math/DHJmath/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/math/DHJmath/config/auth/dto/OAuthAttributes.java
@@ -35,13 +35,24 @@ public class OAuthAttributes {
 
     // 로그인 진행 서비스가 어디인지 구분하여 알맞게 매핑
     // registrationId : OAuth2 로그인을 처리한 서비스 구분 값 ("google", "naver", "kakao")
-    // userNameAttributeName : OAuth2 로그인 시 키가 되는 필드 값. 구글의 키 값은 'sub', 네이버의 키 값은 'response', 카카오의 키 값은 'id'
+    // userNameAttributeName : OAuth2 로그인 시 키가 되는 필드 값.
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 반환해야함
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+
+        if("naver".equals(registrationId)) {
+            return ofNaver(userNameAttributeName, attributes);
+        }
+
+        if("kakao".equals(registrationId)){
+            return ofKakao(userNameAttributeName, attributes);
+        }
+
+
         return ofGoogle(userNameAttributeName, attributes);
     }
+
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName,
                                             Map<String, Object> attributes) {
@@ -53,6 +64,35 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String,Object> response = (Map<String, Object>)attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) response.get("profile");
+        return OAuthAttributes.builder()
+                .name((String)profile.get("nickname"))
+                .email((String)response.get("email"))
+                .picture((String)profile.get("profile_image_url"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+
 
     // User 엔티티 생성
     public User toEntity() {
@@ -66,3 +106,37 @@ public class OAuthAttributes {
     }
 
 }
+
+
+
+// 카카오 JSON
+//{
+//        "id": ----,
+//        "connected_at": "2020-08-15T16:08:16Z",
+//        "properties": {
+//        "nickname": "----",
+//        "profile_image": ""----",
+//        "thumbnail_image": "----"
+//        },
+//        "kakao_account": {
+//        "profile_needs_agreement": false,
+//        "profile": {
+//        "nickname": "---",
+//        "thumbnail_image_url": "----",
+//        "profile_image_url": "----"
+//        },
+//        "has_email": true,
+//        "email_needs_agreement": false,
+//        "is_email_valid": true,
+//        "is_email_verified": true,
+//        "email": "ventulus95@gmail.com",
+//        "has_age_range": false,
+//        "age_range_needs_agreement": false,
+//        "has_birthday": false,
+//        "birthday_needs_agreement": false,
+//        "has_gender": true,
+//        "gender_needs_agreement": false,
+//        "gender": "male"
+//        }
+//        }
+//
